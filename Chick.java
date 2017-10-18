a69c75ac528ecffa22fc0e723433e09b4df3903b
Melinda Li <MeLi5@sfusd.edu>
2017-10-18 14:37:35 -0700

    Everything but NamedCow

--- Chick.java
+++ Chick.java
@@ -1,4 +1,25 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String myType;
+  private String mySound;
+  private String mySound2;
+  public Chick(String type, String sound) {
+    myType = type;
+    mySound = sound;
+  }
+  public Chick(String type, String sound1, String sound2) {
+    myType = type;
+    mySound = sound1;
+    mySound2 = sound2;
+  }
+  public String getSound() {
+    double randSound = Math.random();
+    if (randSound < 0.5) {
+      return mySound;
+    }
+    else {return mySound2;}
+  }
+  public String getType() {
+    return myType;
+  }
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String myName;
+  private String myType;
+  private String mySound;
+  public NamedCow (String type, String name, String sound) {
+    myName = name;
+    myType = type;
+    mySound = sound;
+  }  
+  public String getName() {
+    return myName;
+  }
 }
